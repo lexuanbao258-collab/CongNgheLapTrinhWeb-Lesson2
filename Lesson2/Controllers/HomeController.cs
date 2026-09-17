e9b96dfdf9716f01c38a7122ea49731900afef58
lexuanbao258-collab <lexuanbao258@gmail.com>
2026-09-17 19:35:02 +0700

    lesson 2

---
+++ Lesson2/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Lesson2.Models;
+
+namespace Lesson2.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return Content("Welcome to ASP.NET MVC");
+    }
+
+    public IActionResult About()
+    {
+        return Content("Lê Xuân Bảo");
+    }
+
+    public IActionResult Contact()
+    {
+        return Content("your-email@gmail.com");
+    }
+}
